e41127278d5e0b6557c99746326d7ab11c741960
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-03 20:49:18 +0200

    done
    File: frontend/src/components/layout/AdminDashboardLayout.tsx

--- frontend/src/components/layout/AdminDashboardLayout.tsx
+++ frontend/src/components/layout/AdminDashboardLayout.tsx
@@ -10,11 +10,14 @@ const AdminDashboardLayout: React.FC<AdminDashboardLayoutProps> = ({ children })
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     return (
-        <div className="min-h-screen bg-slate-50 flex flex-col">
-            <AdminTopNav onMenuClick={() => setIsSidebarOpen(true)} />
-            <div className="flex flex-1">
-                <AdminSidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
-                <main className="flex-1 w-full">
+        <div className="min-h-screen bg-slate-50 flex">
+            
+            <AdminSidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+
+            <div className="flex flex-col flex-1 min-w-0">
+                <AdminTopNav onMenuClick={() => setIsSidebarOpen(true)} />
+                
+                <main className="flex-1 p-4 lg:p-8">
                     {children}
                 </main>
             </div>
