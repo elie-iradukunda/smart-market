0435cb06cc2539b61709132b1c267b1b009fce56
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-03 20:49:46 +0200

    donde
    File: frontend/src/pages/StaffDashboard.jsx

--- frontend/src/pages/StaffDashboard.jsx
+++ frontend/src/pages/StaffDashboard.jsx
@@ -1,9 +1,19 @@
 import DashboardLayout from '@/components/layout/DashboardLayout'
-
+import ProductionDashboard from "./ProductionDashboard"
+import InventoryDashboard from "./InventoryDashboard"
+import TechnicianDashboard from "./TechnicianDashboard"
+import ReceptionDashboard from "./ReceptionDashboard"
+import SupportDashboard from "./SupportDashboard"
 const StaffDashboard = () => {
   return (
     <DashboardLayout>
       
+       <ProductionDashboard/>
+
+       <InventoryDashboard/>
+       <TechnicianDashboard/>
+       <ReceptionDashboard/>
+       <SupportDashboard/>
     </DashboardLayout>
   )
 }
